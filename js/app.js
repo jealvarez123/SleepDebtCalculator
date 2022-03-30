651f18bdbc965e7438ff39f9d07d2a6efd78d787
Jaime Alvarez <jealvarez123@gmail.com>
2022-03-30 13:07:59 -0500

    added two functions to the project

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 const getSleepHours = day => {
-
+  day = day.toLowerCase();
+  if (day === 'monday') {
+    return 8;
+  } else if (day === 'tuesday') {
+    return 5;
+  } else if (day === 'wednesday') {
+    return 10;
+  } else if (day === 'thursday') {
+    return 8;
+  } else if (day === 'friday') {
+    return 7;
+  } else if (day === 'saturday') {
+    return 4;
+  } else if (day === 'sunday') {
+    return 9;
+  }
 };
+// This adds up all the hours of the week using a concise body form
+const getActualSleepHours = () =>
+getSleepHours('monday') +
+getSleepHours('tuesday') +
+getSleepHours('wednesday') +
+getSleepHours('thursday') +
+getSleepHours('friday') +
+getSleepHours('saturday') +
+getSleepHours('sunday');
